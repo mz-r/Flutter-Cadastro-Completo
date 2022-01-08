f793917512b93558b095c62462e87a4d958e3b7e
mz-r <mizaelrodriguesworkspace@gmail.com>
2022-01-08 19:32:12 -0300

    first commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,52 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User (
+    id: '1',
+    name: 'Maria',
+    email: 'maria@alunos.com.br',
+    avatarUrl:'https://cdn.pixabay.com/photo/2019/05/28/03/03/ashe-4234217_960_720.jpg',
+  ),
+  '2': const User (
+    id: '2',
+    name: 'Rafael',
+    email: 'rafael@alunos.com.br',
+    avatarUrl:'https://cdn.pixabay.com/photo/2018/12/21/21/07/funko-3888763_960_720.jpg',
+  ),
+  '3': const User (
+    id: '3',
+    name: 'Fernanda',
+    email: 'fernanda@alunos.com.br',
+    avatarUrl:'https://cdn.pixabay.com/photo/2019/06/07/02/33/antiope-wonder-woman-4257409_960_720.jpg',
+  ),
+  '4': const User (
+    id: '4',
+    name: 'Melissa',
+    email: 'melissa@alunos.com.br',
+    avatarUrl:'https://cdn.pixabay.com/photo/2019/05/23/03/14/nebula-guardian-4223088_960_720.jpg',
+  ),
+  '5': const User (
+    id: '5',
+    name: 'Pedro',
+    email: 'pedro@alunos.com.br',
+    avatarUrl:'https://cdn.pixabay.com/photo/2019/05/27/14/26/the-collector-4232836_960_720.jpg',
+  ),
+  '6': const User (
+    id: '6',
+    name: 'João',
+    email: 'joão@alunos.com.br',
+    avatarUrl:'https://cdn.pixabay.com/photo/2019/05/28/16/47/the-gathering-chandra-nalaar-4235553_960_720.jpg',
+  ),
+  '7': const User (
+    id: '7',
+    name: 'Yasmin',
+    email: 'yasmin@alunos.com.br',
+    avatarUrl:'https://cdn.pixabay.com/photo/2019/06/03/01/28/etta-candy-4247939_960_720.jpg',
+  ),
+  '8': const User (
+    id: '8',
+    name: 'Bianca',
+    email: 'bianca@alunos.com.br',
+    avatarUrl:'https://cdn.pixabay.com/photo/2019/05/14/16/07/young-4202743_960_720.jpg',
+  ),
+};
